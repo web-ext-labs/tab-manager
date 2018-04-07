e7fb14ead0bcd4b03b5074854c033d2660488ac8
Trishul <trishul.goel@gmail.com>
2018-04-07 19:12:36 +0530

    honmepage implementation

--- background_script.js
+++ background_script.js
@@ -8,15 +8,22 @@ chrome.tabs.onUpdated.addListener((id, changeInfo, tab) => {
   initializePageAction(tab);
 });
 
-// browser.runtime.onMessage.addListener(req => {
-//     const gettingItem = browser.storage.local.get('shivamsTabs');
-//         gettingItem.then((res) => {
-//             const profiles = res.shivamsTabs || [];
-//             profiles.forEach(profile => {
-//                 const li = document.createElement('li');
-//                 li.textContent = profile;
-//                 document.getElementById('profileHolder').appendChild(li)
-//             })
-            
-//         })
-// });
+const shivamsTabs = {
+  Mywork: {
+      name: 'My work',
+      urls: []
+  },
+  Myhome: {
+      name: 'My home',
+      urls: []
+  }
+}
+
+//setting default values if no profile found
+if(browser && browser.storage) {
+  const gettingItem = browser.storage.local.get('shivamsTabs');
+  gettingItem.then((res) => {
+      if(res.shivamsTabs) return false;
+      browser.storage.local.set({ shivamsTabs });
+  }).catch(console.log)
+}
